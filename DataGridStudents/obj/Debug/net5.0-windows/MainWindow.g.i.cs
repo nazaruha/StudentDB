6d1cc8f0238f5aecad77e8311f66a08c0f04c08e
Nazar <fedunnazar20122001@gmail.com>
2022-07-08 20:54:23 +0300

    Don't remember what changings

--- DataGridStudents/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ DataGridStudents/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -125,7 +125,7 @@ namespace DataGridStudents {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DataGridStudents;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DataGridStudents;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

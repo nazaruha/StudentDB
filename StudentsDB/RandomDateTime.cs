7c801acb24188cee69a5f86d594f6f725895b0ba
Nazar <fedunnazar20122001@gmail.com>
2022-07-09 20:03:00 +0300

    add class which randomly generates date

---
+++ StudentsDB/RandomDateTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsDB
+{
+    public class RandomDateTime
+    {
+        DateTime start;
+        Random gen;
+        int range;
+
+        public RandomDateTime()
+        {
+            start = new DateTime(1970, 1, 1);
+            gen = new Random();
+            range = (DateTime.Today - start).Days;
+        }
+
+        public DateTime Next()
+        {
+            return start.AddDays(gen.Next(range)).AddHours(gen.Next(0, 24)).AddMinutes(gen.Next(0, 60)).AddSeconds(gen.Next(0, 60));
+        }
+    }
+}
